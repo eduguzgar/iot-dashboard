1cb35be9ded164487c1ab1c443c423668a9d3100
eduguzgar <eduguzgar@gmail.com>
2021-08-29 02:29:32 +0200

    minor notation fixes

--- gps/geofence.py
+++ gps/geofence.py
@@ -1,7 +1,6 @@
 def point_in_polygon(lat, lon, polygon):
     """Ray-casting algorithm based on:
     http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html"""
-
     x = lon
     y = lat
 
@@ -10,16 +9,13 @@ def point_in_polygon(lat, lon, polygon):
         # i vertex
         xi = polygon[i][1]
         yi = polygon[i][0]
-
         # j vertex
         xj = polygon[i + 1][1]
         yj = polygon[i + 1][0]
-
         # conditions and ecuation of a line
         intersect = ((yi > y) != (yj > y)) and (
             x < (xj - xi) * (y - yi) / (yj - yi) + xi
         )
-
         # if intersections are an odd number, then the point is inside the polygon
         if intersect is True:
             inside = not inside
